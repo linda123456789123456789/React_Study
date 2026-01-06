d3c0ea2713cbe5dc50f607fd1e0a7432cd84ec1c
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-01-06 23:25:02 +0800

    add: comment

--- src/components/Booking2.js
+++ src/components/Booking2.js
@@ -9,12 +9,15 @@ export class Booking2 extends Component {
       data: 'new data'
     })
   }
+  // 只有第一次渲染才會執行
   componentDidMount(){
     console.log('first render')
   }
+  // 每次重新渲染都會執行
   componentDidUpdate(){
     console.log('re render')
   }
+  // 元件被移除之前會執行
   componentWillUnmount(){
     console.log('unmount')
   }
