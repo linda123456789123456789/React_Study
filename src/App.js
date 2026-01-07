d28251846b35b1c3309d5c009f9369e366129466
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-01-07 23:51:01 +0800

    add: comment and install route package

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Memo from "./pages/Memo";
 import MenuList from "./pages/MenuList";
 import Navbar from "./components/Navbar";
 import { HashRouter, Switch, Route } from "react-router-dom";
-
+// 沒有後端就使用 HashRouter
+// 有後端就使用 BrowserRouter
 export default function App() {
   return (
     <div>
       {/* <Memo /> */}
+      {/* HashRouter -> Switch -> Route */}
       <HashRouter>
         <Navbar />
         <Switch>
           <Route path="/" exact>
+          {/* 只有訪問 / 時才顯示 Home，訪問 /menu-list 就不會顯示 */}
             <Home />
           </Route>
           <Route path="/menu-list">
