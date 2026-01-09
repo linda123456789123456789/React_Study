ba81555bb74eed047597cc70ebc9cea24b025ded
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-01-09 23:02:32 +0800

    add: comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import counterReducer from "./counter";
 import isLoggedReducer from "./isLogged";
-import { combineReducers } from "redux";
+import { combineReducers } from "redux"; // 有兩個以上的 reducer 時，要用 combineReducers 來合併
 
 const allReducers = combineReducers({
   counterReducer,
